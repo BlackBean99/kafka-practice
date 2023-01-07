52590d1b9bfbbdebf11fc819722dc3a699470704
이서현 <blackbean@iseohyeon-ui-MacBookPro.local>
2023-01-07 12:10:53 +0900

    feat : Partition distributes RoundRobin Assign

--- kafka-practice/producers/kafka/src/main/java/org/example/ConsumerMTopicRebalance.java
+++ kafka-practice/producers/kafka/src/main/java/org/example/ConsumerMTopicRebalance.java
@@ -1,10 +1,7 @@
 package org.example;
 
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.errors.WakeupException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +21,13 @@ public class ConsumerMTopicRebalance {
 //        bootstrap.servers,
         props.setProperty("bootstrap.servers", "192.168.56.101:9092");
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_mtopic");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-assign");
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         // static group membership
-        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1");
+        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "3");
+        props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, RoundRobinAssignor.class.getName());
+
+//        props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG)
         // KafkaProducer object creation
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
         kafkaConsumer.subscribe(List.of("topic-p3-t1","topic-p3-t2"));
