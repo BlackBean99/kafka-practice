85ff31b196092d2d69a8c7296bbb6ff99559daf4
이서현 <blackbean@iseohyeon-ui-MacBookPro.local>
2023-01-11 00:47:22 +0900

    feat : OrderDTO

---
+++ kafka-practice/practice/src/main/java/com/practice/kafka/consumer/OrderDBHandler.java
@@ -0,0 +1,13 @@
+package com.practice.kafka.consumer;
+
+import java.util.List;
+
+public class OrderDBHandler {
+    public void insertOrders(List<OrderDTO> orders) {
+
+    }
+
+    public void insertOrder(OrderDTO orderDTO) {
+
+    }
+}
